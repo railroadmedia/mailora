7fec72dd32e0578c2c86035f249d0223810ddad1
Jonathan <jonathan@drumeo.com>
2018-12-17 19:49:36 -0800

    create auth-protected route

--- routes/mailora.php
+++ routes/mailora.php
@@ -4,11 +4,25 @@ use Railroad\Mailora\Controllers\MailController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(
-    ['prefix' => 'mailora',],
-    function(){
+    ['prefix' => 'mailora'],
+    function() {
+
         Route::post(
             '/send',
-            MailController::class . '@send'
+            MailController::class . '@sendPublic'
         )->name('mailora.send');
+
+        Route::group(
+            [
+                'prefix' => 'secure',
+                'middleware' => 'is-logged-in',
+            ],
+            function(){
+                Route::post(
+                    '/send',
+                    MailController::class . '@sendSecure'
+                )->name('mailora.secure.send');
+            }
+        );
     }
 );
