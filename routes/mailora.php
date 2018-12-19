5e653629606fcc6fcd6c2826f4b6671555130021
Jonathan <jonathan@drumeo.com>
2018-12-19 11:41:28 -0800

    restore routes file from temporary troubleshooting version

--- routes/mailora.php
+++ routes/mailora.php
@@ -4,12 +4,20 @@ use Railroad\Mailora\Controllers\MailController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(
-    ['prefix' => 'members'],
+    ['prefix' => 'mailora'],
     function() {
+
         Route::group(
-            [],
+            ['prefix' => 'public'],
             function(){
-                Route::post('/mail', MailController::class . '@sendPublic')->name('mailora.public.send');
+                Route::post('/send', MailController::class . '@sendPublic')->name('mailora.public.send');
+            }
+        );
+
+        Route::group(
+            ['prefix' => 'secure', 'middleware' => 'is-logged-in'],
+            function(){
+                Route::post('/send', MailController::class . '@sendSecure')->name('mailora.secure.send');
             }
         );
     }
