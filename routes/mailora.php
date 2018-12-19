b67219ef2321aa0ed94d1c530e2464af4154c13f
Jonathan <jonathan@drumeo.com>
2018-12-19 08:16:13 -0800

    Temporary version of routes for troubleshooting

--- routes/mailora.php
+++ routes/mailora.php
@@ -4,20 +4,12 @@ use Railroad\Mailora\Controllers\MailController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(
-    ['prefix' => 'mailora'],
+    ['prefix' => 'members'],
     function() {
-
         Route::group(
-            ['prefix' => 'public'],
+            [],
             function(){
-                Route::post('/send', MailController::class . '@sendPublic')->name('mailora.public.send');
-            }
-        );
-
-        Route::group(
-            ['prefix' => 'secure', 'middleware' => 'is-logged-in'],
-            function(){
-                Route::post('/send', MailController::class . '@sendSecure')->name('mailora.secure.send');
+                Route::post('/mail', MailController::class . '@sendPublic')->name('mailora.public.send');
             }
         );
     }
