2431cc191dfbf1ce621236686edb82edeeca4208
Jonathan Maingot <jonathan@drumeo.com>
2021-11-30 23:06:48 -0800

    SupportContact Mailable class sets its own views now

--- src/Mail/SupportContact.php
+++ src/Mail/SupportContact.php
@@ -5,6 +5,7 @@ namespace Railroad\Mailora\Mail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
+use Railroad\Mailora\Services\MailService;
 
 class SupportContact extends Mailable
 {
@@ -21,13 +22,15 @@ class SupportContact extends Mailable
     public function __construct($input, $view)
     {
         $this->input = $input;
-        $this->view = $view;
 
-        $this->input = $input;
         if(!empty(current_user())){
+            $this->view = 'support-contact-from-student';
             $this->input['studentName'] = current_user()->getDisplayName();
             $this->input['studentEmail'] = current_user()->getEmail();
             $this->input['studentId'] = current_user()->getId();
+            $this->view = MailService::getView('support-contact-from-student', []);
+        }else{
+            $this->view = MailService::getView('support-contact-from-public', []);
         }
     }
 
